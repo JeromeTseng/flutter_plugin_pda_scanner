eee1f4806fdb6a577d2bf9103ff50db7cc896c0c
曾兴顺 <zengxingshun@yunjinhz.com>
2024-01-21 11:04:58 +0800

    修改.gitignore文件

--- example/lib/pages/home_page.dart
+++ example/lib/pages/home_page.dart
@@ -13,6 +13,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print('初始化咯');
     PdaScanner.initScanner();
     return Scaffold(
       appBar: buildHomeAppBar(),
